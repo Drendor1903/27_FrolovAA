15b00a5471a665517acab91dfc44d70e99c197cb
Andrey Frolov <drendor02@gmail.com>
2025-01-20 15:07:25 +0700

    Написание тестов

---
+++ src/test/java/autotests/controller/DuckCreateTest.java
@@ -0,0 +1,71 @@
+package autotests.controller;
+
+import com.consol.citrus.TestCaseRunner;
+import com.consol.citrus.annotations.CitrusResource;
+import com.consol.citrus.annotations.CitrusTest;
+import com.consol.citrus.testng.spring.TestNGCitrusSpringSupport;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Test;
+
+import static com.consol.citrus.dsl.MessageSupport.MessageBodySupport.fromBody;
+import static com.consol.citrus.http.actions.HttpActionBuilder.http;
+
+public class DuckCreateTest extends TestNGCitrusSpringSupport {
+
+    @Test(description = "Создание уточки из резины")
+    @CitrusTest
+    public void successfulCreateDuckWithMaterialRubber(@Optional @CitrusResource TestCaseRunner runner) {
+        createDuck(runner, "yellow", 0.15, "rubber", "quack", "ACTIVE");
+
+        validateResponse(runner, "{" + "  \"id\": " + "${duckId}" + ","
+                + "  \"color\": \"" + "yellow" + "\","
+                + "  \"height\": " + 0.15 + ","
+                + "  \"material\": \"" + "rubber" + "\","
+                + "  \"sound\": \"" + "quack" + "\","
+                + "  \"wingsState\": \"" + "ACTIVE"
+                + "\"" + "}");
+    }
+
+    //Создание уточки
+    @Test(description = "Создание уточки из дерева")
+    @CitrusTest
+    public void successfulCreateDuckWithMaterialWood(@Optional @CitrusResource TestCaseRunner runner) {
+        createDuck(runner, "yellow", 0.15, "wood", "quack", "ACTIVE");
+
+        validateResponse(runner, "{" + "  \"id\": " + "${duckId}" + ","
+                + "  \"color\": \"" + "yellow" + "\","
+                + "  \"height\": " + 0.15 + ","
+                + "  \"material\": \"" + "wood" + "\","
+                + "  \"sound\": \"" + "quack" + "\","
+                + "  \"wingsState\": \"" + "ACTIVE"
+                + "\"" + "}");
+    }
+
+    public void validateResponse(TestCaseRunner runner, String responseMessage) {
+        runner.$(http().client("http://localhost:2222")
+                .receive()
+                .response(HttpStatus.OK)
+                .message()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .extract(fromBody().expression("$.id", "duckId"))
+                .body(responseMessage));
+    }
+
+    public void createDuck(TestCaseRunner runner, String color, double height, String material, String sound, String wingsState) {
+        runner.$(
+                http().client("http://localhost:2222")
+                        .send()
+                        .post("/api/duck/create")
+                        .message()
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .body("{\n" + "  \"color\": \"" + color + "\",\n"
+                                + "  \"height\": " + height + ",\n"
+                                + "  \"material\": \"" + material + "\",\n"
+                                + "  \"sound\": \"" + sound + "\",\n"
+                                + "  \"wingsState\": \"" + wingsState
+                                + "\"\n" + "}"));
+    }
+
+}
